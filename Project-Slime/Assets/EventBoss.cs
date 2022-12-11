9f8c626c8b5db17047239266065956600b892089
Daniil Pavshinskiy <daniil.pavshinsky@yandex.ru>
2022-12-11 12:47:14 +0300

    Small fix

--- Project-Slime/Assets/EventBoss.cs
+++ Project-Slime/Assets/EventBoss.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class EventBoss : MonoBehaviour
-{
-    public UnityEvent boss;
 
-    private void OnTriggerEnter(Collider other)
+namespace SA
+{
+    public class EventBoss : MonoBehaviour
     {
-        if (other.gameObject.tag == "Player")
-            boss.Invoke();
+        public UnityEvent boss;
+        public InputHandler IP;
+        public StateManager states;
+        public GameObject boss_obj;
+        public CameraManager camMan;
+
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                states.run = false;
+                states.lockOn = true;
+                states.lockOnTarget = boss_obj.GetComponent<EnemyTarget>();
+                states.lockOnTransform = states.lockOnTarget.GetTarget();
+                camMan.lockonTarget = states.lockOnTarget;
+                camMan.lockonTransform = states.lockOnTransform;
+                camMan.lockon = states.lockOn;
+                IP.enabled = false;
+                states.enabled = false;
+                boss.Invoke();
+            }
+        }
     }
+
 }
+
